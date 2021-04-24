ab35748339167c87630253f9b2cb804859071bd7
niracha1999 <nirachaploi.a@gmail.com>
2021-04-24 13:13:00 +0700

    update cors

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,21 +1,21 @@
-const router = require('express').Router();
-const User = require('../models/User');
-const Company = require('../models/Company');
+const router = require("express").Router();
+const User = require("../models/User");
+const Company = require("../models/Company");
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // res.sendFile(__dirname + "/signup.html");
   const id = req.params.id;
 
   const user = (await Company.findById(id)) || (await User.findById(id));
 
   if (!user) {
-    res.send({ message: err });
+    res.send({ message: err});
   }
 
   try {
     res.send(user);
   } catch (err) {
-    console.log('cannot send user');
+    console.log("cannot send user");
   }
 });
 
