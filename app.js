9e9ac1915399c07126b21fc9d9ffba4d3539a588
krtyong <45281684+krtyong@users.noreply.github.com>
2021-05-05 16:19:12 +0700

    Fix cors

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 //jshint esversion: 6
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const port = process.env.PORT || 1000;
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 //Hide db user password
-require("dotenv").config();
+require('dotenv').config();
 // Connect to DB
 // mongoose.connect(
 //     process.env.DB_CONNECT,
@@ -16,13 +23,13 @@ require("dotenv").config();
 // });
 mongoose
   .connect(
-    "mongodb://" +
+    'mongodb://' +
       process.env.COSMOSDB_HOST +
-      ":" +
+      ':' +
       process.env.COSMOSDB_PORT +
-      "/" +
+      '/' +
       process.env.COSMOSDB_DBNAME +
-      "?ssl=true&replicaSet=globaldb",
+      '?ssl=true&replicaSet=globaldb',
     {
       auth: {
         user: process.env.COSMOSDB_USER,
@@ -33,17 +40,17 @@ mongoose
       retryWrites: false,
     }
   )
-  .then(() => console.log("Connection to CosmosDB successful"))
+  .then(() => console.log('Connection to CosmosDB successful'))
   .catch((err) => console.error(err));
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 
 //Import Route
-const auth = require("./routes/auth");
-const bia = require("./routes/bia");
-const ra = require("./routes/ra");
-const userinfo = require("./routes/userinfo");
+const auth = require('./routes/auth');
+const bia = require('./routes/bia');
+const ra = require('./routes/ra');
+const userinfo = require('./routes/userinfo');
 
 //middleware
 app.use('/user', auth);
@@ -53,12 +60,12 @@ app.use('/userinfo', userinfo);
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   //landing at signup.html page
   // res.sendFile(__dirname + "/signup.html");
-  res.send("welcome to risk whale");
+  res.send('welcome to risk whale');
 });
 
 app.listen(port, function () {
-  console.log("Server is running on port 1000");
+  console.log('Server is running on port 1000');
 });
